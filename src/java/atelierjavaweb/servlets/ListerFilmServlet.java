855513194be865880a9daed15627b5afbe049bab
Formation <Formation@10.18.0.76>
2017-11-22 14:30:26 +0100

    - v4.4

---
+++ src/java/atelierjavaweb/servlets/ListerFilmServlet.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atelierjavaweb.servlets;
+
+import atelierjavaweb.entity.Film;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Formation
+ */
+@WebServlet(name = "ListerFilmServlet", urlPatterns = {"/lister_films"})
+public class ListerFilmServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        EntityManagerFactory myPersistence = Persistence.createEntityManagerFactory("PU");
+        EntityManager em = myPersistence.createEntityManager();
+        
+        em.getTransaction().begin();
+        
+        Query query = em.createQuery("SELECT f FROM Film f");
+        List<Film> films = (List<Film>) query.getSingleResult();
+        
+        String message ="C'est mon message";
+        
+        req.setAttribute("test", message);
+        req.getRequestDispatcher("liste_films.jsp").forward(req, resp);
+        
+    }
+    
+
+}
---
+++ web/liste_films.jsp
@@ -0,0 +1,22 @@
+<%-- 
+    Document   : liste_films
+    Created on : 22 nov. 2017, 12:07:20
+    Author     : Formation
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Liste des films</h1>
+        ${test}
+        <c:forEach items="${films}" var="film">
+            ${film.titre}
+        </c:forEach>
+    </body>
+</html>
